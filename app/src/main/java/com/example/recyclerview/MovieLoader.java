0fb05e090d851f17e0aa7bcfacfd671ac862e2fa
Ryan Freeman <info@ryanfreeman.io>
2020-05-15 00:55:56 +0100

    Added constructor to MovieLoader

--- app/src/main/java/com/example/recyclerview/MovieLoader.java
+++ app/src/main/java/com/example/recyclerview/MovieLoader.java
@@ -1,9 +1,16 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.loader.content.AsyncTaskLoader;
 
 public class MovieLoader extends AsyncTaskLoader<String> {
+    public MovieLoader(@NonNull Context context) {
+        super(context);
+    }
+
     @Nullable
     @Override
     public String loadInBackground() {
