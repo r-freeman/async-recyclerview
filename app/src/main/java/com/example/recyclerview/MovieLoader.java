7379b387f14c1826b59a8dc7b8c60ff6d36b2ffa
Ryan Freeman <info@ryanfreeman.io>
2020-05-15 00:55:18 +0100

    Added loadInBackground to MovieLoader

--- app/src/main/java/com/example/recyclerview/MovieLoader.java
+++ app/src/main/java/com/example/recyclerview/MovieLoader.java
@@ -1,6 +1,12 @@
 package com.example.recyclerview;
 
+import androidx.annotation.Nullable;
 import androidx.loader.content.AsyncTaskLoader;
 
 public class MovieLoader extends AsyncTaskLoader<String> {
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return null;
+    }
 }
